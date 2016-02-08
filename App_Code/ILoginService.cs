441630f797f663c7d5872e8d7545cde5728b4479
caress05 <caress_roche@yahoo.com>
2016-02-08 12:24:59 -0800

    CommitLoginService

---
+++ App_Code/ILoginService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ILoginService" in both code and config file together.
+[ServiceContract]
+public interface ILoginService
+{
+	[OperationContract]
+	int VenueLogin(string password, string username);
+
+    [OperationContract]
+    int VenueRegistration(VenueLite v);
+}
+
+[DataContract]
+public class VenueLite
+{
+    [DataMember]
+    public string Name { set; get; }
+
+    [DataMember]
+    public string Address { set; get; }
+
+    [DataMember]
+    public string City { set; get; }
+
+    [DataMember]
+    public string State { set; get; }
+
+    [DataMember]
+    public string ZipCode { set; get; }
+
+    [DataMember]
+    public string Phone { set; get; }
+
+    [DataMember]
+    public string Email { set; get; }
+
+    [DataMember]
+    public string WebPage { set; get; }
+
+    [DataMember]
+    public int AgeRestriction { set; get; }
+
+    [DataMember]
+    public string UserName { set; get; }
+
+    [DataMember]
+    public string Password { set; get; }
+
+}
